5ce62624d1a14b270698e72c3fd66f1172d162d0
Michael White <cclloyd9786@gmail.com>
2013-11-04 20:01:59 -0500

    Update

--- Homework Tests/ReadWriteDemo.java
+++ Homework Tests/ReadWriteDemo.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
 import java.io.*;
 
-public class FileWriteDemo
+public class ReadWriteDemo
 {
    public static void main(String[] args) throws IOException
    {
       Scanner keyboard = new Scanner(System.in);
       int testnum = 1;
-
+      String filename;
+   
+      System.out.println("How many tests do you have?");  // Prompts the user for the number of tests
+      int testmax = keyboard.nextInt();
+      keyboard.nextLine();
+      System.out.println("What would you like the file to be named? (Do not includ extension)");  // Asks the user for a filename
+      filename = keyboard.nextLine() + ".txt";
+      
       // ===== Writing the file =====
-      PrintWriter testFile = new PrintWriter("myTests.txt");
+      PrintWriter testFile = new PrintWriter(filename);
       System.out.println("What test score #" + testnum + "?");
       testFile.println(keyboard.nextDouble());
-      for (int x=1; x<10; x++) {
+      for (int x = 1; x < testmax; x++) {
         testnum = testnum + 1;
         System.out.println("What test score #" + testnum + "?");
         testFile.println(keyboard.nextDouble());
@@ -21,9 +28,12 @@ public class FileWriteDemo
       System.out.println("Finished writing data to the file. \n\n");
       
       // ===== Appending the file =====
-      FileWriter openFile = new FileWriter("myTests.txt", true);
+     System.out.println("How many tests would you like to add to the list?");
+     testmax = keyboard.nextInt();
+      
+      FileWriter openFile = new FileWriter(filename, true);
       PrintWriter testFile2 = new PrintWriter(openFile);
-      for (int x=1; x<=5; x++) {
+      for (int x = 1; x <= testmax; x++) {
         testnum = testnum + 1;
         System.out.println("What test score #" + testnum + "?");
         testFile2.println(keyboard.nextDouble());
@@ -31,7 +41,7 @@ public class FileWriteDemo
       testFile2.close(); 
       
       // ===== Reading the file =====
-      File testRead = new File("myTests.txt");
+      File testRead = new File(filename);
       Scanner readFile = new Scanner(testRead);
       while (readFile.hasNext() == true){
         System.out.println("Score: " + readFile.nextDouble());
